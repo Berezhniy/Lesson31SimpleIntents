55d623e1e315cb385edc680d5a89707620fa96db
Radik <berezhnoy.rodion.andr0id@gmail.com>
2019-09-29 21:10:01 +0300

    Initial commit

---
+++ app/src/main/java/ru/startandroid/lesson31simpleintents/MainActivity.java
@@ -0,0 +1,56 @@
+package ru.startandroid.lesson31simpleintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnWeb;
+    Button btnMap;
+    Button btnCall;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //нахожу view по id
+        btnWeb = (Button) findViewById(R.id.btnWeb);
+        btnMap = (Button) findViewById(R.id.btnMap);
+        btnCall = (Button) findViewById(R.id.btnCall);
+        //вешаю слушатель
+        btnWeb.setOnClickListener(this);
+        btnMap.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //создаю intent
+        Intent intent;
+        //создаю свич по 3 кнопкам
+        switch (v.getId()) {
+            //при нажатии будет открываться ссылка
+            case R.id.btnWeb:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
+                startActivity(intent);
+                break;
+            case R.id.btnMap:
+                //при нажатии будет открываться карта с определённой локацией
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:55.754283,37.62002"));
+                startActivity(intent);
+                break;
+            case R.id.btnCall:
+                //при нажатии будет открываться набор номера с уже ведённым номером
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:12345"));
+                startActivity(intent);
+                break;
+        }
+    }
+}
